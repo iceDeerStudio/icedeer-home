a481e5a6aa867a4388162b904632baeec317eea2
WiseBrightness <1255437975@qq.com>
2024-03-06 17:19:19 +0800

    SVG属性问题报错修复

--- app/ui/navigation.jsx
+++ app/ui/navigation.jsx
@@ -40,9 +40,9 @@ export default function Navigation({ pages, contacts }) {
                         viewBox='0 0 24 24'
                         fill='none'
                         stroke='currentColor'
-                        stroke-width='2'
-                        stroke-linecap='round'
-                        stroke-linejoin='round'
+                        strokeWidth='2'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
                     >
                         <line x1='4' x2='20' y1='12' y2='12' />
                         <line x1='4' x2='20' y1='6' y2='6' />
